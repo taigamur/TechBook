172e91335c372eae9882aa7a411f7057629a1846
Taiga Miura <miura.t84@gmail.com>
2022-02-10 21:29:27 +0900

    ホーム画面追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,24 @@
 class HomeController < ApplicationController
-    skip_before_action :check_logged_in, only: :index
+    # skip_before_action :check_logged_in, only: :index
+
+    def index
+
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def update
+    end
+
+    def edit
+        current_user
+    end
+
+
+    
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
         if(user = User.find_or_create_from_auth_hash(auth_hash))
             log_in user
         end
-        redirect_to root_path
+        redirect_to home_index_path
     end
 
     def destroy
